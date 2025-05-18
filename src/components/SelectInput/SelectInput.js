827bfe84d49e9008a096673850790e504dba5a2f
MaidHalilovic <maidhaliloviclimunada567@gmail.com>
2025-05-18 21:44:46 +0200

    Starting With Questions

---
+++ src/components/SelectInput/SelectInput.js
@@ -0,0 +1,46 @@
+import React from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+
+const SelectInput = () => {
+  const [difficulty, setDifficulty] = React.useState("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setDifficulty(event.target.value);
+  };
+  return (
+    <div
+    //   style={{
+    //     display: "flex",
+    //     justifyContent: "center",
+    //     flexDirection: "column",
+    //   }}
+    >
+      <h2>First choose difficulty</h2>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <FormControl sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel id='demo-simple-select-autowidth-label'>
+            Difficulty
+          </InputLabel>
+          <Select
+            labelId='demo-simple-select-autowidth-label'
+            id='demo-simple-select-autowidth'
+            value={difficulty}
+            onChange={handleChange}
+            autoWidth
+            label='Difficulty'
+          >
+            <MenuItem value=''>{/* <em>None</em> */}</MenuItem>
+            <MenuItem value={20}>Easy</MenuItem>
+            <MenuItem value={21}>Medium</MenuItem>
+            <MenuItem value={22}>Hard</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+    </div>
+  );
+};
+
+export default SelectInput;
--- src/pages/VideoGames/VideoGames.js
+++ src/pages/VideoGames/VideoGames.js
@@ -1,11 +1,15 @@
 import React from "react";
+import "./videoGames.css";
+import SelectInput from "../../components/SelectInput/SelectInput";
 
 const VideoGames = () => {
   return (
     <div className='quiz'>
       <div className='instructions'>
-        <h1>20 question for video games </h1>
-        <h3>See you in the end</h3>
+        <h1>20 questions for video games </h1>
+        <h2>Lets see how points you will get:</h2>
+
+        <SelectInput />
       </div>
     </div>
   );
