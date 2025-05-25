737ab597abeff2648e874a9b0223516a7e9c8ff8
MaidHalilovic <maidhaliloviclimunada567@gmail.com>
2025-05-25 18:12:25 +0200

    Made first 3 questions for video games

--- src/pages/VideoGames/VideoGames.js
+++ src/pages/VideoGames/VideoGames.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import "./videoGames.css";
-import SelectInput from "../../components/SelectInput/SelectInput";
 import axios from "axios";
 
 const VideoGames = () => {
@@ -9,7 +8,7 @@ const VideoGames = () => {
   const fetchQuestions = async () => {
     try {
       const { data } = await axios.get(
-        `https://opentdb.com/api.php?amount=20&category=15`
+        `https://opentdb.com/api.php?amount=20&category=15&type=multiple`
       );
 
       console.log(data);
@@ -33,7 +32,33 @@ const VideoGames = () => {
       <div className='instructions'>
         <h1>20 questions for video games </h1>
         <h2>Lets see how points you will get:</h2>
-        <h1>{questions.results[0].question}</h1>
+        <div className='questions'>
+          <h2>{questions.results[0].question}</h2>
+          <div className='answers'>
+            <button>{questions.results[0].incorrect_answers[0]}</button>
+            <button>{questions.results[0].incorrect_answers[1]}</button>
+            <button>{questions.results[0].incorrect_answers[2]}</button>
+            <button>{questions.results[0].correct_answer}</button>
+          </div>
+        </div>
+        <div className='questions'>
+          <h2>{questions.results[1].question}</h2>
+          <div className='answers'>
+            <button>{questions.results[1].incorrect_answers[0]}</button>
+            <button>{questions.results[1].incorrect_answers[1]}</button>
+            <button>{questions.results[1].incorrect_answers[2]}</button>
+            <button>{questions.results[1].correct_answer}</button>
+          </div>
+        </div>
+        <div className='questions'>
+          <h2>{questions.results[2].question}</h2>
+          <div className='answers'>
+            <button>{questions.results[2].incorrect_answers[0]}</button>
+            <button>{questions.results[2].incorrect_answers[1]}</button>
+            <button>{questions.results[2].incorrect_answers[2]}</button>
+            <button>{questions.results[2].correct_answer}</button>
+          </div>
+        </div>
       </div>
     </div>
   );
